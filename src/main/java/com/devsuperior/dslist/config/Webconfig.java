7327343d2f5693d04d54be893194650026c1e71d
Daniel Nascimento <daniel.p.nasciment@gmail.com>
2025-05-23 17:17:55 -0300

    CORS config

---
+++ src/main/java/com/devsuperior/dslist/config/Webconfig.java
@@ -0,0 +1,25 @@
+package com.devsuperior.dslist.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+public class Webconfig {
+
+    @Value("${cors.origins}")
+    private String corsOrigins;
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedMethods("*").allowedOrigins(corsOrigins);
+            }
+        };
+    }
+
+}
+//Configuração de restrição de acesso apenas por um endereço/url
+// onde acesso por outra url ele o cors irá bloquear
